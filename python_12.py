f056603b922640be2498869b57ab9262f7766a6f
AnanyaA148 <138643111+AnanyaA148@users.noreply.github.com>
2024-05-21 18:42:02 -0400

    problem 13

--- python_12.py
+++ python_12.py
@@ -1,12 +1,13 @@
 check =0
-count = 1
+count = 0
 num =1
 while(check < 500 ):
     check =0
     count +=1
-    num += count
+    num = count *(count+1)//2
 
     for i in range(num):
         if num % (i+1) ==0:
             check +=1
+
 print(num)
