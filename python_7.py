66bca4f20647b2ec3d415edee313fcabba1d4633
AnanyaA148 <138643111+AnanyaA148@users.noreply.github.com>
2024-05-20 22:17:17 -0400

    fast solve for problem 7

--- python_7.py
+++ python_7.py
@@ -1,18 +1,25 @@
+import math
+lst = [2]
+check =1
 num =2
 count = 1
 while(count < 10001):
     prime = False
     num = num+1
-    for i in range(2, num):
-        if num % i ==0:
-            prime = False
-            break
-        else:
+    i = 0
+    for check in lst:
+        if check < math.sqrt(num):
+            if num % check ==0:
+                prime = False
+                break
+            else:
+                prime = True
+            i = i+1
 
-            prime = True
 
     if prime:
         count = count +1
+        lst.append(num)
 
 print(num)
 
