84fc11bfc323c7972de4b99c2bb75f4d96fec8df
AnanyaA148 <138643111+AnanyaA148@users.noreply.github.com>
2024-05-20 21:22:44 -0400

    Solved Problem 5

--- python_5.py
+++ python_5.py
@@ -1,10 +1,15 @@
 count = 1
+num = count
 done = False
+track =1
 while not done:
-    count = count + 1
+    count = count + num
     for i in range(20):
         if count % (i+1) != 0:
             done = False
+            if i > track:
+                track = i+1
+                num = count
             break
         else:
             done = True
